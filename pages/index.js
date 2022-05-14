f222a240f504da6b82ae98d6f4f03d3826277324
greta <greta.workman@gmail.com>
2022-05-14 17:46:12 -0400

    updates to dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import useSWR from 'swr';
 
 export default function Home() {
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+  const { data } = useSWR('/api/spotify', fetcher);
   return (
     <div className={styles.container}>
       <Head>
@@ -15,9 +18,15 @@ export default function Home() {
         <h1 className={styles.title}>
           Greta Workman
         </h1>
-
-        
+        <>
+      <section className='bg-gray-600'>
+        <main className='flex items-center justify-center'>
+          {console.log(data)}
+        </main>
+      </section>
+    </>
       </main>
+
 
       <footer className={styles.footer}>
         <a
