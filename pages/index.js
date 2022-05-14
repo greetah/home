cc38239d4003d6339a4b8f8f194615392c104ea6
greta <greta.workman@gmail.com>
2022-05-14 18:47:16 -0400

    getting rid of target blank

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <a
-          target='_blank'
-          rel="noreferer noopener"
           href={
             data?.isPlaying
             ? data.songUrl
@@ -51,8 +49,6 @@ export default function Home() {
     <footer className={styles.footer}>
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noreferrer noopener"
         >
         Powered by{' '}
         <span className={styles.logo}>
