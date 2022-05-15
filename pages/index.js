8175d6ab8aae3ea85a76cb5cee43addcdc183272
greta <greta.workman@gmail.com>
2022-05-15 13:05:10 -0400

    adding stuff

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import RecentlyPlayed from './api/RecentlyPlayed';
 
-export async function Home() {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
