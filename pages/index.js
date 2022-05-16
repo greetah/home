e2a02b3d2f4ae8e00bf680ea01b620b514293b06
greta <greta.workman@gmail.com>
2022-05-16 18:36:51 -0400

    main

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home() {
         <h1 className={styles.title}>
           Greta Workman
         </h1>
-    </main>
       <div className='flex flex-col items-center justify-center min-h-screen space-y-3'>
       <a
       href={
@@ -53,6 +52,7 @@ export default function Home() {
       </div>
     </a>
   </div>
+  </main>
       <div><footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
