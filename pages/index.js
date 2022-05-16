e7a37be7239dd3a1d897167fce9718d31be5238d
greta <greta.workman@gmail.com>
2022-05-16 18:01:10 -0400

    defining sispotify

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import useSWR from 'swr';
+import { SiSpotify } from 'react-icons/si';
 import RecentlyPlayed from './api/RecentlyPlayed';
 
 export default function Home() {
