61afd351d4b732a72fdaf3f226b8b3990890c5e0
greta <greta.workman@gmail.com>
2022-05-17 08:59:45 -0400

    changed the api

--- pages/api/NowPlaying.js
+++ pages/api/NowPlaying.js
@@ -1,5 +1,3 @@
-import querystring from 'querystring';
-
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
@@ -10,30 +8,30 @@ const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
     const response = await fetch(TOKEN_ENDPOINT, {
-        method: 'POST',
-        headers: {
-            Authorization: `Basic ${basic}`,
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: querystring.stringify({
-            grant_type: 'refresh_token',
-            refresh_token,
-        }),
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${basic}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token
+      })
     });
-
+  
     return response.json();
-};
-
-export const getNowPlaying = async () => {
+  };
+  
+  export const getNowPlaying = async () => {
     const { access_token } = await getAccessToken();
   
     return fetch(NOW_PLAYING_ENDPOINT, {
       headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
+        Authorization: `Bearer ${access_token}`
+      }
     });
   };
-  
+
   export default async (_, res) => {
     const response = await getNowPlaying();
   
@@ -57,4 +55,4 @@ export const getNowPlaying = async () => {
     };
   
     res.status(200).json(data);
-  };
+  }
