a2c94cddf295464146d25e14cf1c55aa80394c33
greta <greta.workman@gmail.com>
2022-05-21 16:54:47 -0400

    i feel like i just did this

--- pages/api/nowplaying.js
+++ pages/api/nowplaying.js
@@ -1,28 +1,27 @@
 import querystring from 'querystring';
 import { getNowPlaying } from 'lib/spotify';
 
-export const getNowPlaying = async () => {
-  const { access_token } = await getAccessToken();
-  
-  const song = await response.json();
+export default async (_, res) => {
+  const response = await getNowPlaying();
 
-  if (song.item === null) {
-  return res.status(200).json({ isPlaying: false });
-}
+  if (response.status === 204 || response.status > 400) {
+      return res.status(200).json({ isPlaying: false });
+  }
 
-    const isPlaying = song.is_playing;
-    const title = song.item.name;
-    const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
-    const album = song.item.album.name;
-    const albumImageUrl = song.item.album.images[0].url;
-    const songUrl = song.item.external_urls.spotify;
+      const song = await response.json();
+      const isPlaying = song.is_playing;
+      const title = song.item.name;
+      const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
+      const album = song.item.album.name;
+      const albumImageUrl = song.item.album.images[0].url;
+      const songUrl = song.item.external_urls.spotify;
 
-return res.status(200).json({
-  album,
-  albumImageUrl,
-  artist,
-  isPlaying,
-  songUrl,
-  title
-});
-}
+  return res.status(200).json({
+      album,
+      albumImageUrl,
+      artist,
+      isPlaying,
+      songUrl,
+      title,
+  });
+};
