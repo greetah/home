1dc4827f4563ed43229f1b0d1cc2dff1e00e15b3
greta <greta.workman@gmail.com>
2022-05-21 16:28:49 -0400

    what does this do

--- pages/api/now-playing.js
+++ pages/api/now-playing.js
@@ -1,15 +1,28 @@
 import useSWR from 'swr';
+import { getNowPlaying } from 'lib/spotify';
 
-export default function Home() {
-  const fetcher = (url) => fetch(url).then((r) => r.json());
-  const { data } = useSWR('/api/spotify', fetcher);
-  return (
-    <>
-      <section className='bg-gray-600'>
-        <main className='flex justify-center items-center'>
-          {console.log(data)}
-        </main>
-      </section>
-    </>
-  );
-}
+
+export default async (_, res) => {
+  const response = await getNowPlaying();
+
+  if (
+    response.status === 204 ||
+    response.status > 400 ||
+    response.data.currently_playing_type !== 'track'
+  ) {
+    return res.status(200).json({ isPlaying: false });
+  }
+
+  const data = {
+    isPlaying: response.data.is_playing,
+    title: response.data.item.name,
+    album: response.data.item.album.name,
+    artist: response.data.item.album.artists
+      .map((artist) => artist.name)
+      .join(', '),
+    albumImageUrl: response.data.item.album.images[0].url,
+    songUrl: response.data.item.external_urls.spotify,
+  };
+
+  res.status(200).json(data);
+};
