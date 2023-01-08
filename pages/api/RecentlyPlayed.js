03218e7b325d103e1cf434d9106c1d2a6254f9d9
Greta Workman <84729203+greetah@users.noreply.github.com>
2023-01-08 13:15:57 -0500

    Update RecentlyPlayed.js

--- pages/api/RecentlyPlayed.js
+++ pages/api/RecentlyPlayed.js
@@ -1,13 +1,11 @@
 import querystring from 'querystring';
 
-const {
-    SPOTIFY_CLIENT_ID: client_id,
-    SPOTIFY_CLIENT_SECRET: client_secret,
-    SPOTIFY_REFRESH_TOKEN: refresh_token,
-} = process.env;
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
-const RECENTLY_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/recently-played`;
+const RECENTLY_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
@@ -33,30 +31,5 @@ export const getRecentlyPlayed = async () => {
         headers: {
             Authorization: `Bearer ${access_token}`,
         },
-    });
-};
-
-export function (res) => {
-    const response = await getRecentlyPlayed();
-
-    if (response.status === 204 || response.status > 400) {
-        return res.status(200).json({ isPlaying: false });
-    }
-
-    const song = response.json();
-    const isPlaying = song.is_playing;
-    const title = song.item.name;
-    const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
-    const album = song.item.album.name;
-    const albumImageUrl = song.item.album.images[0].url;
-    const songUrl = song.item.external_urls.spotify;
-
-    return res.status(200).json({
-        album,
-        albumImageUrl,
-        artist,
-        isPlaying,
-        songUrl,
-        title,
     });
 };
