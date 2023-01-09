c702a8ae5052c3265d8016d4b7c0b8e75a8f2e46
Greta Workman <greta.workman@gmail.com>
2023-01-08 20:35:14 -0500

    next font working

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import "../styles/globals.css"
+import "../styles/globals.css";
+import { Space_Grotesk } from "@next/font/google";
+
+const space = Space_Grotesk({ subsets: ["latin"] });
 
 export default function MyApp({ Component, pageProps }) {
-  
-  return <Component {...pageProps} />
+  return (
+    <main className={space.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class',
-  
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       black: colors.black,
       white: colors.white,
       gray: colors.gray,
@@ -26,9 +27,9 @@ module.exports = {
       yellow: colors.yellow,
     },
     fontFamily: {
-      sans: ['Inter', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-      mono: ['ui-monospace', 'SFMono-Regular']
-    }
-  }
-  }
+      sans: ["Space Grotesk", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+    },
+  },
+};
