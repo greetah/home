ed19b433cabd176682b2d9ab202c76cb18178598
Greta Workman <greta.workman@gmail.com>
2023-01-08 21:11:27 -0500

    all the hover states

--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,23 @@ export default function Home() {
   return (
     <div className="max-w-5xl mx-auto px-4">
       <div className="flex flex-col sm:flex-row items-start py-8">
-        <div className="mr-8">
+        <div className="sm:mr-8">
           <Image
-            className="rounded-lg w-full sm:w-auto"
+            className="shadow-lg transition-shadow duration-200 rounded-lg w-full sm:w-auto hover:shadow-2xl"
             width={250}
             height={307}
             src="/gretaworkman.jpg"
           />
         </div>
 
-        <div className="flex flex-col">
-          <h1 className="font-bold text-3xl sm:text-4xl tracking-tight mb-1 text-black dark:text-white">
+        <div>
+          <h1 className="-mx-2 px-2 font-bold text-3xl sm:text-4xl mb-1 hover:bg-pink-600 hover:shadow hover:rounded hover:font-extrabold">
             Greta Workman
           </h1>
-          <h2 className="text-gray-700 dark:text-gray-200 mb-4">
+          <h2 className="text-gray-700 mb-4">
             Product Marketing @ <span className="font-semibold">Vercel</span>
           </h2>
-          <p className="text-gray-600 dark:text-gray-400 mb-16">
+          <p className="text-gray-600 mb-16">
             Here is where I write a sick bio
           </p>
           <div className="w-full grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
