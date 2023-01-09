4b5b9fb042ef3b95826c30ab332c8a72de15cc23
Greta Workman <greta.workman@gmail.com>
2023-01-08 22:05:43 -0500

    empty commit messages are the bane of my existance

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,31 @@ export default function Home() {
           content="Director of Product Marketing at Vercel"
         />
       </Head>
+
+      <div className="justify-between flex flex-col sm:flex-row items-start py-10">
+        <div className="sm:mr-8">
+          <Image
+            className="shadow-lg transition-shadow duration-200 rounded-lg w-full sm:w-auto hover:shadow-2xl"
+            width={250}
+            height={307}
+            src="/gretaworkman.jpg"
+          />
+        </div>
+        <div>
+          <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold mb-1 -ml-[1px]">
+            Greta Workman
+          </h1>
+          <h2 className="text-gray-700 text-lg mb-4">
+            Product Marketing @{" "}
+            <a className="my-underline font:semibold" href="https://vercel.com">
+              Vercel
+            </a>
+          </h2>
+          {/* <p className="text-gray-600 mb-16">
+            
+          </p> */}
+        </div>
+      </div>
       <div className="flex justify-center p-4 sm:flex-row sm:space-y-0 sm:space-x-4 space-y-4">
         <div className="flex flex-col sm:flex-row sm:space-y-0 sm:space-x-4 space-y-4">
           <div>
@@ -31,31 +56,6 @@ export default function Home() {
               GitHub
             </a>
           </div>
-        </div>
-      </div>
-      <div className="justify-between flex flex-col sm:flex-row items-start py-10">
-        <div className="sm:mr-8">
-          <Image
-            className="shadow-lg transition-shadow duration-200 rounded-lg w-full sm:w-auto hover:shadow-2xl"
-            width={250}
-            height={307}
-            src="/gretaworkman.jpg"
-          />
-        </div>
-
-        <div>
-          <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold mb-1 -ml-[1px]">
-            Greta Workman
-          </h1>
-          <h2 className="text-gray-700 text-lg mb-4">
-            Product Marketing @{" "}
-            <a className="my-underline font:semibold" href="https://vercel.com">
-              Vercel
-            </a>
-          </h2>
-          {/* <p className="text-gray-600 mb-16">
-            
-          </p> */}
         </div>
       </div>
     </div>
