8de477f08330fcad72cecab0b369d3a32578ed65
Greta Workman <greta.workman@gmail.com>
2023-01-08 23:56:05 -0500

    fix endpoint

--- pages/api/spotify.js
+++ pages/api/spotify.js
@@ -5,7 +5,7 @@ const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
-const RECENTLY_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
